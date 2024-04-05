c7fca93dbe3b2425b4789555889cd74ba68e333c
RahulSolanki007 <36640123+RahulSolanki007@users.noreply.github.com>
2024-04-05 05:57:32 +0530

    basics of java script

---
+++ basics1.js
@@ -0,0 +1,28 @@
+console.log("Hello World")
+
+let a = 4
+/* var to declare a variable until ES5
+from ES6 we have Let and const also along with var to declare variable */
+console.log(a) //to print varibale value 
+// these are comments
+/*
+multiple line comments
+
+*/
+// Data types
+console.log(typeof(a))
+let b = 235.46
+console.log(typeof(b)) //number
+
+var c = "Rahul"
+console.log(typeof(c)) // String
+
+let required = true
+console.log(typeof(required)) // boolean
+
+
+var c = a+b
+console.log(c)
+
+// we can not redeclare variable with let keyword but possible with var
+console.log(!required)
